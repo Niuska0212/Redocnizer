d070a714b16d957733f6cda9438a155bc07d1579
Niuska0212 <niuska.gonzalez5462@alumnos.udg.mx>
2025-03-14 02:30:11 -0600

    verificacion y mejora

--- src/preprocesamiento.py
+++ src/preprocesamiento.py
@@ -6,17 +6,24 @@ import cv2  # Para cargar imágenes
 def cargar_datos(directorio_base):
     """Carga imágenes desde un directorio y las convierte en datos numéricos."""
     X, y = [], []
-    etiquetas = {nombre: i for i, nombre in enumerate(sorted(os.listdir(directorio_base)))}
 
-    for etiqueta, indice in etiquetas.items():
+    # Filtra solo carpetas para evitar archivos sueltos
+    etiquetas = {nombre: i for i, nombre in enumerate(sorted(f for f in os.listdir(directorio_base) if os.path.isdir(os.path.join(directorio_base, f))))}
+
+    for etiqueta, indice in etiquetas.items(): #Recorre todas las carpetas A-Z, 0-9
         carpeta = os.path.join(directorio_base, etiqueta)
-        if os.path.isdir(carpeta):
-            for archivo in os.listdir(carpeta):
+        if os.path.isdir(carpeta): #Verifica que es una carpeta
+            for archivo in os.listdir(carpeta): #Lee cada imagen
                 ruta = os.path.join(carpeta, archivo)
                 imagen = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)  # Carga en escala de grises
+
+                if imagen is None: #Verifica si la imagen es valida
+                    print(f"Error al cargar la imagen {ruta}")
+                    continue #Salta esta imagen y sigue con la siguiente
+
                 imagen = cv2.resize(imagen, (28, 28)).flatten()  # Redimensiona y aplana
                 X.append(imagen)
-                y.append(indice)
+                y.append(indice) #USa el nombr ede la carpeta como etiqueta
 
     return np.array(X, dtype=np.float32) / 255.0, np.array(y)
 
@@ -24,3 +31,8 @@ if __name__ == "__main__":
     directorio_training = os.path.join("..", "data", "data", "training_data")
     X, y = cargar_datos(directorio_training)
     print(f"Se cargaron {len(X)} imágenes de entrenamiento.")
+
+
+
+
+
